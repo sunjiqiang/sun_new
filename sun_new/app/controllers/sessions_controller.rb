7821a0d749462f5001f9c5946b30b5495eeed313
sunjiqiang <sunjiqiang@foxmail.com>
2013-02-27 04:44:04 +0800

    first

---
+++ sun_new/app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def login
+  end
+  def new
+  end
+  def register
+    @user=User.create!(:login=>params[:login],:password=>params[:password])
+     if @user
+      flash[:notice] = "Successful registration #{@user.login}"
+      redirect_to login_path
+    else
+      flash[:notice] = " The register shibai"
+      redirect_to login_path
+    end
+  end
+  def create
+    @user = User.authentication(params[:login], params[:password])
+    if @user
+      session[:user_id] = @user.id
+      flash[:notice] = "welcome #{@user.login}"
+      redirect_to posts_path
+    else
+      flash[:notice] = " The login shibai"
+      redirect_to login_path
+    end
+  end
+  
+end
---
+++ sun_new/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SunNew::Application.config.secret_token = '846766f700cc82cb19ba6f98cada43c4b53fbab030fc91e6e137b3ffdc9bafbf34041b41093232473200ff56ff4f9c033bff21b478cab0f60006f11ce6bb8496'
